c69b4fe5d617365873d40bd94e42752c09e729d1
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-10-03 22:15:41 +0900

    Reformat build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-set -e -x
 
-# Cf. https://github.com/conda-forge/staged-recipes/issues/673, we're in the
-# process of excising Libtool files from our packages. Existing ones can break
-# the build while this happens. We have "/." at the end of $PREFIX to be safe
-# in case the variable is empty.
-find ${PREFIX}/. -name '*.la' -delete
+set -e
+
+configure_args=(
+    --prefix="${PREFIX}"
+    --disable-static
+    --disable-dependency-tracking
+    --disable-selective-werror
+    --disable-silent-rules
+)
 
 autoreconf_args=(
     --force
@@ -16,30 +19,18 @@ autoreconf_args=(
 )
 autoreconf "${autoreconf_args[@]}"
 
-export CONFIG_FLAGS="--build=${BUILD}"
+configure_args+=("--build=${BUILD}")
 
 export PKG_CONFIG_LIBDIR="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig"
-configure_args=(
-    ${CONFIG_FLAGS}
-    --disable-debug
-    --disable-dependency-tracking
-    --disable-selective-werror
-    --disable-silent-rules
-    --prefix="${PREFIX}"
-)
 
-if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ] ; then
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]] ; then
     configure_args+=(
         --enable-malloc0returnsnull
     )
 fi
 
 ./configure "${configure_args[@]}"
-make -j${CPU_COUNT}
+make -j$CPU_COUNT
 make install
 
 rm -rf ${PREFIX}/share/man ${PREFIX}/share/doc/${PKG_NAME}
-
-# Remove any new Libtool files we may have installed. It is intended that
-# conda-build will eventually do this automatically.
-find ${PREFIX}/. -name '*.la' -delete
